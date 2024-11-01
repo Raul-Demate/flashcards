c7c531ab5585ab327b8d2b76c7ea3eb0d2d05a0c
Raul-Demate <00001066112186sp@al.educacao.sp.gov.br>
2024-11-01 11:18:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
+)
+
+criaCartao(
+    'Química',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118'
+)
+
+criaCartao(
+    'Inglês',
+    'O que a palavra legend significa em português?',
+    'Lenda'
+)
+
+criaCartao(
+    'Física',
+    'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    '8 minutos'
+)
